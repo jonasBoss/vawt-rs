22c995b5df76e3aca134556b446b0c8f8a8a71b8
Jonas Bosse <jonas.bosse@posteo.de>
2023-08-08 11:12:58 +0200

    aerofoil wip

--- src/areofoil.rs
+++ src/areofoil.rs
@@ -1,5 +1,6 @@
-use ndarray::{Array1, Array3, OwnedRepr, Ix3};
+use ndarray::{Array1, Array3, OwnedRepr, Ix3, Array};
 use ndarray_interp::{interp2d::{Interp2D, Biliniar}, Interp2DVec};
+use thiserror::Error;
 
 #[derive(Debug)]
 /// Coefficient of lift and drag
@@ -33,17 +34,68 @@ impl Aerofoil {
 #[derive(Debug)]
 pub struct AerofoilBuilder{
     /// Look up tabel for [cl, cd] over Re, alpha
-    lut: Vec<Vec<[f64;2]>>,
-    alpha: Vec<f64>,
-    re: Vec<f64>,
+    lut: Option<Array3<f64>>,
+    alpha: Option<Array1<f64>>,
+    re: Option<Array1<f64>>,
+    symmetric: bool,
 }
 
 impl AerofoilBuilder {
     pub fn new() -> Self {
-        AerofoilBuilder { lut: Vec::new(), alpha: Vec::new(), re: Vec::new() }
-    } 
-    
-    pub fn build() -> Aerofoil{
+        AerofoilBuilder { lut: None, alpha: None, re: None, symmetric: true }
+    }
+
+    pub fn data(mut self, cl_cd: Array3<f64>) -> Self {
+        self.lut = Some(cl_cd);
+        self
+    }
+
+    pub fn alpha(mut self, alpha: Array1<f64>) -> Self{
+        self.alpha = Some(alpha);
+        self
+    }
+
+    pub fn re(mut self, re: Array1<f64>) -> Self {
+        self.re = Some(re);
+        self
+    }
+
+    pub fn symmetric(mut self, yes: bool) -> Self{
+        self.symmetric = yes;
+        self
+    }
+
+    pub fn build(self) -> Result<Aerofoil, AerofoilBuildError>{
+        let AerofoilBuilder { lut, alpha, re, symmetric } = self;
+        let lut = match lut {
+            Some(lut) => lut,
+            None => return Err(AerofoilBuildError::MissingData),
+        }; 
+        let alpha = match alpha {
+            Some(alpha) => alpha,
+            None => return Err(AerofoilBuildError::MissingAlpha),
+        };
+        let re = match re {
+            Some(re) => re,
+            None => return Err(AerofoilBuildError::MissingRe),
+        };
+
+        if !lut.shape()[2] == 2 {
+
+        };
+
         todo!()
     }
+}
+
+#[derive(Debug, Error)]
+pub enum AerofoilBuildError {
+    #[error("No aerofil data was provided")]
+    MissingData,
+    #[error("No angle of attac data was provided")]
+    MissingAlpha,
+    #[error("No raynolds nurbers was provided")]
+    MissingRe,
+    #[error("")]
+    WrongDataShape,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
+use std::{fs::File, collections::VecDeque};
+
+use csv::{ReaderBuilder, StringRecord};
+use ndarray::{Array2, Axis, Array3, s, Slice, Array1, array, concatenate, stack};
+use ndarray_csv::Array2Reader;
+
 mod areofoil;
 
 fn main() {
-    println!("Hello, world!");
+    let mut files = VecDeque::from(["foo/cldAR/NACA0018Re0040.data", "foo/cldAR/NACA0018Re0080.data", "foo/cldAR/NACA0018Re0160.data"]);
+    let re = array![40_000.0, 80_000.0, 160_000.0];
+
+    let arrays: Vec<_> = files.into_iter().map(read_array).collect();
+    let alpha = arrays[0].index_axis(Axis(1), 0).into_owned();
+    let arrays_slice: Vec<_> = arrays.iter().map(|a|a.view()).collect();
+    let mut data = stack(Axis(1), arrays_slice.as_slice()).unwrap();
+    data.slice_axis_inplace(Axis(2), Slice::from(1..));
+
+    println!("{data:?}");
+    println!("{alpha:?}");
+}
+
+
+fn read_array(path: &str) -> Array2<f64> {
+    let file = File::open(path).unwrap();
+    let mut reader = ReaderBuilder::new()
+        .has_headers(false)
+        .trim(csv::Trim::All)
+        .delimiter(b',')
+        .from_reader(file);
+
+    reader.deserialize_array2_dynamic().unwrap()
 }
