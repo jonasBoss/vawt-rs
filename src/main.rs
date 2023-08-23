8400efe013386067f0ed613cd9bcb3ae9221f7d5
Jonas Bosse <jonas.bosse@posteo.de>
2023-08-23 15:20:36 +0200

    streamlined main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, f64::consts::PI};
+use std::{fs::File, f64::consts::PI, error::Error};
 
 use csv::ReaderBuilder;
 use ndarray::{array, Array2, Axis, Array, s};
@@ -6,45 +6,31 @@ use ndarray_csv::Array2Reader;
 
 use vawt::{areofoil::Aerofoil, turbine::{VAWTSolver, Verbosity, Turbine}, streamtube::StreamTube};
 
-fn main() {
-    let files = Vec::from([
-        "foo/cldAR/NACA0018Re0080.data",
-        "foo/cldAR/NACA0018Re0040.data",
-        "foo/cldAR/NACA0018Re0160.data",
-    ]);
-    let re = array![80_000.0, 40_000.0, 160_000.0];
-
-    let mut builder = Aerofoil::builder();
-    for (file, re) in files.into_iter().zip(re) {
-        builder.add_data_row(read_array(file), re).unwrap();
-    }
-
-    let aerofoil = builder
+fn main() -> Result<(), Box<dyn Error>> {
+    let aerofoil = Aerofoil::builder()
+        .add_data_row(read_array("examples/NACA0018/NACA0018Re0080.data")?, 80_000.0)?
+        .add_data_row(read_array("examples/NACA0018/NACA0018Re0040.data")?, 40_000.0)?
+        .add_data_row(read_array("examples/NACA0018/NACA0018Re0160.data")?, 160_000.0)?
         .set_aspect_ratio(12.8)
         .update_aspect_ratio(true)
         .symmetric(true)
-        .build()
-        .unwrap();
-    //println!("{aerofoil:?}")
+        .build()?;
 
-    // let mut solver = VAWTSolver::new(&aerofoil);
-
-    // solver.re(31_300.0)
-    //     .solidity(0.3525)
-    //     .n_streamtubes(100)
-    //     .verbosity(Verbosity::Silent);
-    // let solution = solver.solve_with_beta(0.0);
-
-    //println!("{solution:#?}");
-
-    let turbine = Turbine { re: 31_300.0, tsr: 3.0, solidity: 0.3525, aerofoil: &aerofoil };
-    let tube = StreamTube::new(PI * 3.0 / 2.0, 0.0, 0.0);
-    let w_a_re = tube.w_alpha_re(0.0, &turbine);
-    println!("{w_a_re:?}")
+    let n = 72;
+    let d_t_half = PI / n as f64;
+    let theta = Array::linspace(d_t_half, 2.0 * PI - d_t_half, n);
+    
+    let mut solver = VAWTSolver::new(&aerofoil);
+    solver.re(31_300.0)
+        .solidity(0.3525)
+        .n_streamtubes(n)
+        .verbosity(Verbosity::Silent);
+    let solution = solver.tsr(3.25).solve_with_beta(0.0);
+    Ok(())
 }
 
-fn read_array(path: &str) -> Array2<f64> {
-    let file = File::open(path).unwrap();
+fn read_array(path: &str) -> Result<Array2<f64>, Box<dyn Error>> {
+    let file = File::open(path)?;
     let mut reader = ReaderBuilder::new()
         .has_headers(false)
         .trim(csv::Trim::All)
@@ -55,5 +41,5 @@ fn read_array(path: &str) -> Array2<f64> {
     for mut datapoint in arr.axis_iter_mut(Axis(0)) {
         datapoint[0] = datapoint[0].to_radians();
     }
-    arr
+    Ok(arr)
 }
