0553ede5012b9aa4e6f31dab2f60f7e5b667c1e2
Jonas Bosse <jonas.bosse@posteo.de>
2023-09-21 17:33:46 +0200

    main for perf measurements

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,34 @@
-use std::{error::Error, f64::consts::PI, fs::File};
+use std::{error::Error, fs::File, time::Instant};
 
 use csv::ReaderBuilder;
 
-use ndarray::{Array, Array2, Axis};
+use ndarray::{Array2, Axis};
 use ndarray_csv::Array2Reader;
 
 use vawt::{areofoil::Aerofoil, VAWTSolver};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::init();
+    let aerofoil = load_naca_0018()?;
+    let testcase = setup_solver(&aerofoil);
 
-    let aerofoil = Aerofoil::builder()
-        .add_data_row(
-            read_array("examples/NACA0018/NACA0018Re0080.data")?,
-            80_000.0,
-        )?
-        .add_data_row(
-            read_array("examples/NACA0018/NACA0018Re0040.data")?,
-            40_000.0,
-        )?
-        .add_data_row(
-            read_array("examples/NACA0018/NACA0018Re0160.data")?,
-            160_000.0,
-        )?
+    let start = Instant::now();
+    for _ in 0..10_000 {
+        testcase.solve_with_beta(0.0);
+    }
+    let duration = start.elapsed();
+    println!("Duration for 10'000 solutions: {} microseconds", duration.as_micros());
+    Ok(())
+}
+
+fn load_naca_0018() -> Result<Aerofoil, Box<dyn Error>> {
+    Ok(Aerofoil::builder()
+        .add_data_row(read_array("tests/NACA0018/NACA0018Re0080.data")?, 80_000.0)?
+        .add_data_row(read_array("tests/NACA0018/NACA0018Re0040.data")?, 40_000.0)?
+        .add_data_row(read_array("tests/NACA0018/NACA0018Re0160.data")?, 160_000.0)?
         .set_aspect_ratio(12.8)
         .update_aspect_ratio(true)
         .symmetric(true)
-        .build()?;
-
-    let n = 72;
-    let d_t_half = PI / n as f64;
-    let _theta = Array::linspace(d_t_half, 2.0 * PI - d_t_half, n);
-
-    let mut solver = VAWTSolver::new(&aerofoil);
-    solver.re(31_300.0).solidity(0.3525).n_streamtubes(n);
-    let _solution = solver.tsr(3.25).solve_optimize_beta();
-    Ok(())
+        .build()?)
 }
 
 fn read_array(path: &str) -> Result<Array2<f64>, Box<dyn Error>> {
@@ -51,4 +44,14 @@ fn read_array(path: &str) -> Result<Array2<f64>, Box<dyn Error>> {
         datapoint[0] = datapoint[0].to_radians();
     }
     Ok(arr)
+}
+
+fn setup_solver(aerofoil: &Aerofoil) -> VAWTSolver{
+    let mut testcase = VAWTSolver::new(&aerofoil);
+    testcase
+        .re(31_300.0)
+        .solidity(0.3525)
+        .n_streamtubes(72)
+        .tsr(3.25);
+    testcase
 }
